a77b6127b3d93c82c3ff1e31ba49b3db194d41a6
23f3000111 <mahtorishikesh93@gmail.com>
2025-09-08 09:49:11 +0530

    Aded BE3.1_HW1 folder

---
+++ BE3.1_HW1/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express JS")
+})
+
+app.get("/products", (req, res) => {
+  res.send("Browse our products here.")
+})
+
+app.get("/services", (req, res) => {
+  res.send("Explore our services.")
+})
+
+app.get("/faq", (req, res) => {
+  res.send("Frequently Asked Questions.")
+})
+
+app.get("/gallery", (req, res) => {
+  res.send("View our gallery.")
+})
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT)
+})
